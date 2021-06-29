13271eb6a94343ebca0e983cb983b9bb3b09ec8e
jay_desktop <seoljaehong8@gmail.com>
2021-06-29 16:11:21 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="hello world" />
+  <HelloWorld msg="테스트 성공" />
   <!-- <HelloWorld msg="Welcome to Jaehong Seol every App" /> -->
 </template>
 
